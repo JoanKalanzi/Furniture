421c6064b124711d43a0dbf219682854aac1babe
Joan kalanzi <72227085+JoanKalanzi@users.noreply.github.com>
2023-07-10 10:33:41 +0100

    error handling

--- src/components/Show/Products/FurnitureList.js
+++ src/components/Show/Products/FurnitureList.js
@@ -8,7 +8,7 @@ const classes = useStyles();
   return (
     <main className={classes.content}>
     <div className={classes.toolbar} />
-    <Grid container justify="center" spacing={4}>
+    <Grid container justifyContent="center" spacing={4}>
       {data.map((product) => (
         <Grid key={product.id} item xs={12} sm={6} md={4} lg={3}>
           <FurnitureCard product={product} onAddToCart={onAddToCart} />
