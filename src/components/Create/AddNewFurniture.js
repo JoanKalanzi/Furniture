da95dbe067f6e73d176d91152184e7e2c1401711
Dan <dan@Dans-MacBook-Air.local>
2023-07-10 12:49:02 +0100

    changed Home.css import to home.css, also updated create file

--- src/components/Create/AddNewFurniture.js
+++ src/components/Create/AddNewFurniture.js
@@ -1,86 +1,134 @@
 import React, { useState } from 'react';
 import './AddNewFurniture.css';
+
 const AddFurniture = ({ onAddFurniture }) => {
-  const [name, setName] = useState('');
-  const [description, setDescription] = useState('');
-  const [price, setPrice] = useState('');
-  const [image, setImage] = useState('');
+  const [furnitureType, setFurnitureType] = useState('');
+  const [colour, setColour] = useState('');
+  const [material, setMaterial] = useState('');
+  const [priceRange, setPriceRange] = useState('');
+  const [picture, setPicture] = useState('');
+  const [additionalDescription, setAdditionalDescription] = useState('');
+  const [submitted, setSubmitted] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const newFurniture = {
-      name,
-      description,
-      price,
-      image,
+      furnitureType,
+      colour,
+      material,
+      priceRange,
+      picture,
+      additionalDescription,
     };
     onAddFurniture(newFurniture);
-    setName('');
-    setDescription('');
-    setPrice('');
-    setImage('');
+    setFurnitureType('');
+    setColour('');
+    setMaterial('');
+    setPriceRange('');
+    setPicture('');
+    setAdditionalDescription('');
+    setSubmitted(true);
   };
 
   return (
     <div className="add-furniture">
       <h2 className="add-furniture-heading">Add New Furniture</h2>
-      <form className="add-furniture-form" onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label className="form-label" htmlFor="name">
-            Name
-          </label>
-          <input
-            className="form-input"
-            type="text"
-            id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </div>
+      {submitted ? (
+        <p>Thank you for your request. Someone from the team will look into this and get back to you soon.</p>
+      ) : (
+        <form className="add-furniture-form" onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label className="form-label" htmlFor="furnitureType">
+              Furniture Type
+            </label>
+            <select
+              className="form-input"
+              id="furnitureType"
+              value={furnitureType}
+              onChange={(e) => setFurnitureType(e.target.value)}
+            >
+              <option value="">Select furniture type</option>
+              <option value="Chair">Chair</option>
+              <option value="Table">Table</option>
+              <option value="Sofa">Sofa</option>
+              <option value="Bed">Bed</option>
+            </select>
+          </div>
 
-        <div className="form-group">
-          <label className="form-label" htmlFor="description">
-            Description
-          </label>
-          <input
-            className="form-input"
-            type="text"
-            id="description"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-          />
-        </div>
+          <div className="form-group">
+            <label className="form-label" htmlFor="colour">
+              Colour
+            </label>
+            <input
+              className="form-input"
+              type="text"
+              id="colour"
+              value={colour}
+              onChange={(e) => setColour(e.target.value)}
+            />
+          </div>
 
-        <div className="form-group">
-          <label className="form-label" htmlFor="price">
-            Price
-          </label>
-          <input
-            className="form-input"
-            type="text"
-            id="price"
-            value={price}
-            onChange={(e) => setPrice(e.target.value)}
-          />
-        </div>
+          <div className="form-group">
+            <label className="form-label" htmlFor="material">
+              Material
+            </label>
+            <input
+              className="form-input"
+              type="text"
+              id="material"
+              value={material}
+              onChange={(e) => setMaterial(e.target.value)}
+            />
+          </div>
 
-        <div className="form-group">
-          <label className="form-label" htmlFor="image">
-            Image URL
-          </label>
-          <input
-            className="form-input"
-            type="text"
-            id="image"
-            value={image}
-            onChange={(e) => setImage(e.target.value)}
-          />
-        </div>
+          <div className="form-group">
+            <label className="form-label" htmlFor="priceRange">
+              Price Range
+            </label>
+            <select
+              className="form-input"
+              id="priceRange"
+              value={priceRange}
+              onChange={(e) => setPriceRange(e.target.value)}
+            >
+              <option value="">Select price range</option>
+              <option value="£0-150">£0-150</option>
+              <option value="£151-500">£151-500</option>
+              <option value="£501-700">£501-700</option>
+              <option value="£700+">£700 +</option>
+            </select>
+          </div>
 
-        <button className="form-submit-btn" type="submit">
-          Add Furniture
-        </button>
-      </form>
+          <div className="form-group">
+            <label className="form-label" htmlFor="picture">
+              Picture URL
+            </label>
+            <input
+              className="form-input"
+              type="text"
+              id="picture"
+              value={picture}
+              onChange={(e) => setPicture(e.target.value)}
+            />
+          </div>
+
+          <div className="form-group">
+            <label className="form-label" htmlFor="additionalDescription">
+              Additional Description
+            </label>
+            <textarea
+              className="form-input"
+              id="additionalDescription"
+              value={additionalDescription}
+              onChange={(e) => setAdditionalDescription(e.target.value)}
+            />
+          </div>
+
+          <button className="form-submit-btn" type="submit">
+            Add Furniture
+          </button>
+        </form>
+      )}
     </div>
   );
 };
