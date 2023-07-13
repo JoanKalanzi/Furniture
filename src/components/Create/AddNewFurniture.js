a88c68db2b3c83b874fabff00b848e9d49553451
Joan kalanzi <72227085+JoanKalanzi@users.noreply.github.com>
2023-07-13 12:15:33 +0100

    final

--- src/components/Create/AddNewFurniture.js
+++ src/components/Create/AddNewFurniture.js
@@ -30,13 +30,8 @@ const AddFurniture = () => {
  
 
   return (
-
-    <><div className="section-head col-sm-12">
-      <h4><span>Custom </span> Order</h4>
-    </div><div className="center-form">
-        <form className="signup-container" onSubmit={handleSubmit}>
-          <div className="left-container">
-
+    <div className="section-head col-sm-12">
+            <h4><span>Custom </span> Order</h4>
     <div className="center-form">
       <form className="signup-container" onSubmit={handleSubmit}>
         <div className="left-container">
@@ -47,48 +42,9 @@ const AddFurniture = () => {
             className="mylogo"
           />
           <div className="puppy">
-
             <img
-              src={process.env.PUBLIC_URL + Logo}
+              src={process.env.PUBLIC_URL + CheckImg}
               alt="logo"
-
-              width={'1%'}
-              className="mylogo" />
-            <div className="puppy">
-              <img
-                src={process.env.PUBLIC_URL + CheckImg}
-                alt="logo"
-                width={'40%'}
-                className="checkout-image"
-                style={{ float: 'left', marginRight: '10px' }} />
-            </div>
-          </div>
-          <div className="right-container">
-            <header>
-              <h1>Struggling to find what you need? Discover the perfect match with our personalized form!</h1>
-            </header>
-            <div className="set">
-              <div className="pets-name">
-                <label htmlFor="pets-name">Furniture Type</label>
-                <input type="text" id="pets-name" placeholder="Sofa" value={furnitureType} onChange={(e) => setFurnitureType(e.target.value)} />
-              </div>
-              <div className="pets-photo">
-                <button id="pets-upload">
-                  <AiFillCamera />
-                </button>
-                <label htmlFor="pets-upload">Upload a photo</label>
-              </div>
-            </div>
-            <div className="set">
-              <div className="pets-breed">
-                <label htmlFor="pets-breed">Colour</label>
-                <input type="text" id="pets-breed" placeholder="Velvet" value={colour} onChange={(e) => setColour(e.target.value)} />
-              </div>
-              <div className="pets-birthday">
-                <label htmlFor="pets-birthday">Material</label>
-                <input type="text" id="pets-birthday" placeholder="Teal" value={material} onChange={(e) => setMaterial(e.target.value)} />
-              </div>
-
               width={'40%'}
               className="checkout-image"
               style={{ float: 'left', marginRight: '10px' }}
@@ -119,42 +75,43 @@ const AddFurniture = () => {
             <div className="pets-birthday">
               <label htmlFor="pets-birthday">Material</label>
               <input type="text" id="pets-birthday" placeholder="Teal"  />
-
-            </div>
-            <div className="set">
-              <div className="pets-gender">
-                <label htmlFor="pet-gender-female">Storage Required:</label>
-                <div className="radio-container">
-                  <input type="radio" id="pet-gender-female" name="pet-gender" value="female" checked />
-                  <label htmlFor="pet-gender-female">Yes</label>
-                  <input type="radio" id="pet-gender-male" name="pet-gender" value="male" />
-                  <label htmlFor="pet-gender-male">No</label>
-                </div>
-              </div>
-              <div className="pets-spayed-neutered">
-                <label htmlFor="pet-spayed">Additional Info:</label>
-                <input type="text" id="additional-info" placeholder="More Info" name="additional-info" />
-              </div>
-            </div>
-            <div className="pets-weight">
-              <label htmlFor="pet-weight-0-25">Price Range</label>
-              <div className="radio-container">
-                <input type="radio" id="pet-weight-0-25" name="pet-weight" value="£0-150" checked />
-                <label htmlFor="pet-weight-0-25">£0-150</label>
-                <input type="radio" id="pet-weight-25-50" name="pet-weight" value="£151-500" />
-                <label htmlFor="pet-weight-25-50">£151-500</label>
-                <input type="radio" id="pet-weight-50-100" name="pet-weight" value="£501-700" />
-                <label htmlFor="pet-weight-50-100">£501-700</label>
-                <input type="radio" id="pet-weight-100-plus" name="pet-weight" value="£700+" />
-                <label htmlFor="pet-weight-100-plus">£700 +</label>
-              </div>
-            </div>
-            <div className="check-bnt">
-              <button className="checkout-button" type="submit">Submit</button>
             </div>
           </div>
-        </form>
-      </div></>
+          <div className="set">
+            <div className="pets-gender">
+              <label htmlFor="pet-gender-female">Storage Required:</label>
+              <div className="radio-container">
+                <input type="radio" id="pet-gender-female" name="pet-gender" value="female" checked />
+                <label htmlFor="pet-gender-female">Yes</label>
+                <input type="radio" id="pet-gender-male" name="pet-gender" value="male" />
+                <label htmlFor="pet-gender-male">No</label>
+              </div>
+            </div>
+            <div className="pets-spayed-neutered">
+              <label htmlFor="pet-spayed">Additional Info:</label>
+              <input type="text" id="additional-info" placeholder="More Info" name="additional-info" />
+            </div>
+          </div>
+          <div className="pets-weight">
+            <label htmlFor="pet-weight-0-25">Price Range</label>
+            <div className="radio-container">
+              <input type="radio" id="pet-weight-0-25" name="pet-weight" value="£0-150" checked />
+              <label htmlFor="pet-weight-0-25">£0-150</label>
+              <input type="radio" id="pet-weight-25-50" name="pet-weight" value="£151-500" />
+              <label htmlFor="pet-weight-25-50">£151-500</label>
+              <input type="radio" id="pet-weight-50-100" name="pet-weight" value="£501-700" />
+              <label htmlFor="pet-weight-50-100">£501-700</label>
+              <input type="radio" id="pet-weight-100-plus" name="pet-weight" value="£700+" />
+              <label htmlFor="pet-weight-100-plus">£700 +</label>
+            </div>
+          </div>
+          <div className="check-bnt">
+            <button className="checkout-button" type="submit">Submit</button>
+          </div>
+        </div>
+      </form>
+    </div>
+    </div>
   );
   }
 
