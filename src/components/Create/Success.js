7fcf0c484822265d1ab48ade789df75dfc4e43b7
Dan <dan@Dans-MacBook-Air.local>
2023-07-13 11:02:46 +0100

    completed create functionality and success page

--- src/components/Create/Success.js
+++ src/components/Create/Success.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import './success.css';
+import Img3 from '../images/img8.jpeg';
+import { Link } from 'react-router-dom';
 
 const Success = () => {
-    return (
-        <p>Successful</p>
-
-
-    )
-
+  return (
+    <div className="center-form">
+      <div className="success-container">
+        <div className="form-container">
+          <img
+            src={process.env.PUBLIC_URL + Img3}
+            alt=""
+            className="success-image"
+          />
+          <div className="message-container">
+            <h2 className='success-h2'>Thank you for your custom request!</h2>
+            <p>Our team will carefully review your request</p>
+            <p>and aim to provide a response within 3-5 business days.</p>
+            <p className="click-message">To shop some more, click:</p>
+            <Link to="/show" className="success-button">Store</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Success;
